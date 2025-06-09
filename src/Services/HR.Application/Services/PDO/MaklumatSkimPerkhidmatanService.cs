bd76550156b0b096ebe7e8473557329e958c9d3a
nityanandyadav <91151343+nityanandyadav@users.noreply.github.com>
2025-06-09 20:43:44 +0530

    Nitya bug fix09 june25 (#19)
    * api/pdo/MaklumatSkimPerkhidmatan/getSenaraiSkimPerkhidmatan
    
    Bug fixed for this API
    
    * Changed in API

--- src/Services/HR.Application/Services/PDO/MaklumatSkimPerkhidmatanService.cs
+++ src/Services/HR.Application/Services/PDO/MaklumatSkimPerkhidmatanService.cs
@@ -35,9 +35,40 @@ namespace HR.Application.Services.PDO
 
                 _logger.LogInformation("Getting all MaklumatSkimPerkhidmatanDto using EF Core join");
 
-                var query = (from a in _dbContext.PDOKumpulanPerkhidmatan
-                             join b in _dbContext.PDOStatusPermohonanKumpulanPerkhidmatan
-                                 on a.Id equals b.IdKumpulanPerkhidmatan
+                //            var query = (from a in _dbContext.PDOKumpulanPerkhidmatan
+                //                         join b in _dbContext.PDOStatusPermohonanKumpulanPerkhidmatan
+                //                             on a.Id equals b.IdKumpulanPerkhidmatan
+                //                         join b2 in _dbContext.PDORujStatusPermohonan
+                //                             on b.KodRujStatusPermohonan equals b2.Kod
+                //                         where b.StatusAktif == true
+                //                         orderby a.Kod
+                //                         select new
+                //                         {
+                //                             a.Kod,
+                //                             a.Nama,
+                //                             a.Keterangan,
+                //                             a.StatusAktif,
+                //                             StatusPermohonanNama = b2.Nama,
+                //                             b.TarikhKemaskini,
+                //                             a.Id
+                //                         })
+                //.AsEnumerable()
+                //.Select((x, index) => new
+                //{
+                //    Bil = index + 1,
+                //    x.Kod,
+                //    x.Nama,
+                //    x.Keterangan,
+                //    StatusSkimPerkhidmatan = x.StatusAktif ? "Aktif" : "Tidak Aktif",
+                //    StatusPermohonan = x.StatusPermohonanNama,
+                //    x.TarikhKemaskini,
+                //    x.Id
+                //});
+                var query = (from a in _dbContext.PDOSkimPerkhidmatan
+                             join a2 in _dbContext.PDORujStatusSkim
+                                 on a.KodRujStatusSkim equals a2.Id
+                             join b in _dbContext.PDOStatusPermohonanSkimPerkhidmatan
+                                 on a.Id equals b.IdSkimPerkhidmatan
                              join b2 in _dbContext.PDORujStatusPermohonan
                                  on b.KodRujStatusPermohonan equals b2.Kod
                              where b.StatusAktif == true
@@ -47,31 +78,32 @@ namespace HR.Application.Services.PDO
                                  a.Kod,
                                  a.Nama,
                                  a.Keterangan,
-                                 a.StatusAktif,
+                                 a.IdKlasifikasiPerkhidmatan,
+                                 a.IdKumpulanPerkhidmatan,
+                                 StatusSkimPerkhidmatanNama = a2.Nama,
                                  StatusPermohonanNama = b2.Nama,
-                                 b.TarikhKemaskini,
+                                 b.TarikhKemasKini,
                                  a.Id
                              })
-    .AsEnumerable()
-    .Select((x, index) => new
-    {
-        Bil = index + 1,
-        x.Kod,
-        x.Nama,
-        x.Keterangan,
-        StatusSkimPerkhidmatan = x.StatusAktif ? "Aktif" : "Tidak Aktif",
-        StatusPermohonan = x.StatusPermohonanNama,
-        x.TarikhKemaskini,
-        x.Id
-    });
-                // Apply filters
-                //if (!string.IsNullOrWhiteSpace(filter.MaklumatKumpulanPerkhidmatanId))
-                //    query = query.Where(q => q.Nama.Contains(filter.Nama));
+            .AsEnumerable()
+            .Select((x, index) => new
+            {
+                Bil = index + 1,
+                x.Kod,
+                x.Nama,
+                x.Keterangan,
+                x.IdKlasifikasiPerkhidmatan,
+                x.IdKumpulanPerkhidmatan,
+                StatusSkimPerkhidmatan = x.StatusSkimPerkhidmatanNama,
+                StatusPermohonan = x.StatusPermohonanNama,
+                x.TarikhKemasKini,
+                x.Id
+            });
 
-                //if (!string.IsNullOrWhiteSpace(filter.MaklumatKlasifikasiPerkhidmatanId))
-                //    query = query.Where(q => q.Nama.Contains(filter.Nama));
+                if (filter.MaklumatKlasifikasiPerkhidmatanId.HasValue)
+                    query = query.Where(q => q.IdKlasifikasiPerkhidmatan == filter.MaklumatKlasifikasiPerkhidmatanId);
                 if (filter.MaklumatKumpulanPerkhidmatanId.HasValue)
-                    query = query.Where(x => x.Id == filter.MaklumatKumpulanPerkhidmatanId);
+                    query = query.Where(x => x.IdKumpulanPerkhidmatan == filter.MaklumatKumpulanPerkhidmatanId);
 
                 if (!string.IsNullOrWhiteSpace(filter.Kod))
                     query = query.Where(q => q.Kod.Contains(filter.Kod));
@@ -80,7 +112,7 @@ namespace HR.Application.Services.PDO
                     query = query.Where(q => q.Nama.Contains(filter.Nama));                
 
                 if (!string.IsNullOrWhiteSpace(filter.StatusPermohonan))
-                    query = query.Where(q => q.Kod == filter.StatusPermohonan);
+                    query = query.Where(q => q.StatusPermohonan == filter.StatusPermohonan);
 
                 var data = query.ToList();
                 var result = data
@@ -92,7 +124,7 @@ namespace HR.Application.Services.PDO
                         Keterangan = q.Keterangan,
                         StatusSkimPerkhidmatan = q.StatusSkimPerkhidmatan,
                         StatusPermohonan = q.StatusPermohonan,
-                        TarikhKemaskini=q.TarikhKemaskini
+                        TarikhKemaskini=q.TarikhKemasKini
                     })
                     .ToList();
 
---
+++ src/Services/HR.Core/Entities/PDO/PDORujStatusSkim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Core.Entities.PDO
+{
+    [Table("PDO_RujStatusSkim")] // This is the key
+    public class PDORujStatusSkim : PDOBaseEntity
+    {
+        public string Nama { get; set; }
+        public string Keterangan { get; set; }
+    }
+}
