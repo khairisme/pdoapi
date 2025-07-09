ca8e46ace66a323b7195a2157a414c6d1313b755
amar-asn <amar.rng@gmail.com>
2025-07-09 19:34:31 +0530

    PDP Basic structure created

---
+++ src/Services/HR.Core/Entities/PDP/PDPJadualGaji.cs
@@ -0,0 +1,29 @@
+﻿using HR.Core.Entities.PDO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Core.Entities.PDP
+{
+    [Table("PDP_JadualGaji")]
+    public class PDPJadualGaji : PDPBaseEntity
+    {
+        public string? KodJenisSaraan { get; set; }
+        public int? IdGred { get; set; }
+        public int? NomborGred { get; set; }
+        public decimal? GajiMinimum { get; set; }
+        public decimal? GajiMaksimum { get; set; }
+        public decimal? KadarKGT { get; set; }
+        public decimal? KadarKGM { get; set; }
+        public decimal PeratusKGT { get; set; }
+        public string? KodMataGaji { get; set; }
+        public int? PeringkatMataGaji { get; set; }
+        public int? TingkatMataGaji { get; set; }
+        public DateTime TarikhMulaJadualGaji { get; set; }
+        public DateTime TarikhTamatJadualGaji { get; set; }
+        public string? ButiranKemasKini { get; set; }
+    }
+}
---
+++ src/Services/HR.Infrastructure/Data/EntityFramework/PDPDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HR.Core.Entities.PDO;
+using HR.Core.Entities.PDP;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HR.Infrastructure.Data.EntityFramework
+{
+    public class PDPDbContext : DbContext
+    {
+        public PDPDbContext(DbContextOptions<PDPDbContext> options) : base(options)
+        {
+        }
+        public DbSet<PDPJadualGaji> PDPJadualGaji { get; set; }
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PDPJadualGaji>(entity =>
+            {
+                entity.ToTable("PDP_JadualGaji");
+                entity.HasKey(e => e.Id);
+            });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
